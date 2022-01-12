94750ea82a92ba903bc018671d82d535f5d0604e
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-01-12 02:59:25 -0600

    Fix(?) a render state leak
    Seems to fix a lighting leak for the Tweakeroo Hotbar Swap Overlay at least

--- src/main/java/fi/dy/masa/malilib/render/InventoryOverlay.java
+++ src/main/java/fi/dy/masa/malilib/render/InventoryOverlay.java
@@ -490,8 +490,8 @@ public class InventoryOverlay
         mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, stack, 0, 0, null);
         mc.getItemRenderer().zOffset -= 100;
 
-        RenderUtils.disableDiffuseLighting();
         RenderSystem.popMatrix();
+        RenderUtils.color(1f, 1f, 1f, 1f);
     }
 
     public static void renderStackToolTip(int x, int y, ItemStack stack, MinecraftClient mc)
