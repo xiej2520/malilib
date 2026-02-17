82613be02a0b6354eba05bf3d808defb34c83fe9
xiej2520 <xiej2520@gmail.com>
2026-02-16 19:50:16 -0600

    getPointedInventory from cameraEntity instead of player

--- src/main/java/malilib/render/inventory/InventoryRenderUtils.java
+++ src/main/java/malilib/render/inventory/InventoryRenderUtils.java
@@ -70,6 +70,7 @@ import malilib.util.inventory.VanillaInventoryView;
 import malilib.util.position.BlockPos;
 import malilib.util.position.HitResult;
 import malilib.util.position.Vec2i;
+import org.apache.http.util.EntityUtils;
 
 public class InventoryRenderUtils
 {
@@ -503,19 +504,31 @@ public class InventoryRenderUtils
     public static Pair<InventoryView, InventoryRenderDefinition> getPointedInventory()
     {
         World world = WorldWrap.getBestWorld();
+        Entity cameraEntity = GameWrap.getCameraEntity();
         EntityPlayer clientPlayer = GameWrap.getClientPlayer();
 
-        // We need to get the player from the server world,
-        // so that the player itself won't be included in the ray trace
-        EntityPlayer player = world.getPlayerEntityByUUID(EntityWrap.getUuid(clientPlayer));
-
-        if (player == null)
+        if (cameraEntity == clientPlayer && world instanceof WorldServer)
         {
-            player = clientPlayer;
+            // We need to get the player from the server world,
+            // so that the player itself won't be included in the ray trace
+            EntityPlayer player = world.getPlayerEntityByUUID(EntityWrap.getUuid(clientPlayer));
+
+            if (player == null)
+            {
+                cameraEntity = player;
+            }
         }
 
         RayTraceUtils.RayTraceFluidHandling fluidHandling = RayTraceUtils.RayTraceFluidHandling.NONE;
-        HitResult trace = RayTraceUtils.getRayTraceFromEntity(world, player, fluidHandling, true, 6.0);
+        HitResult trace;
+        if (cameraEntity != clientPlayer)
+        {
+            trace = RayTraceUtils.getRayTraceFromEntity(world, cameraEntity, fluidHandling, true, 6.0);
+        }
+        else
+        {
+            trace = GameWrap.getHitResult();
+        }
 
         if (trace == null)
         {
